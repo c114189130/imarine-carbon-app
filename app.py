9669353d8ef667fc284d5defe31bed71d13b36c9
c114189130 <c114189130@nkust.edu.tw>
2026-05-26 17:24:19 +0800

    Remove flask_cors dependency

--- app.py
+++ app.py
@@ -474,6 +474,8 @@ def create_certificate():
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 400
 
-# ================= 啟動 =================
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
